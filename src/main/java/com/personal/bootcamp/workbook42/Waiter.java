cebcbb2c8e4e4e6da78ec22e4193548b35e10d42
karol castro <karolinycosta@yahoo.com.br>
2022-02-16 15:38:41 -0300

    tipTheWaiter

---
+++ src/main/java/com/personal/bootcamp/workbook42/Waiter.java
@@ -0,0 +1,16 @@
+package com.personal.bootcamp.workbook42;
+
+public class Waiter {
+    public static void main(String[] args) {
+        tipTheWaiter(50);
+
+    }
+
+    public static void tipTheWaiter(double bill) {
+        double tip = bill *0.15;
+
+        System.out.println("Waiter: I hope you enjoyed your meal!\n");
+        System.out.println("Thank you!\n" +
+                "Your service was wonderful! Please, accept this tip: $" +tip);
+    }
+}
