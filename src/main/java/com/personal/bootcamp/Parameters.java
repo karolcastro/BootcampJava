1101cb4eb1c700398fe943eb844a0eebe71984a8
karol castro <karolinycosta@yahoo.com.br>
2022-02-16 15:18:35 -0300

    Parameters

--- src/main/java/com/personal/bootcamp/Parameters.java
+++ src/main/java/com/personal/bootcamp/Parameters.java
@@ -2,13 +2,19 @@ package com.personal.bootcamp;
 
 public class Parameters {
     public static void main(String[] args) {
-        measureRectangle(5, 4);
+        measureRectangle(4.3, 2.2);
+        measureRectangle(3.2, 4.1);
+        measureRectangle(3.5, 1.2);
+        measureRectangle(1.6, 5.6);
+        measureRectangle(10.1, 0.7);
+        measureRectangle(20.2, 5.6);
 
     }
 
     public static void measureRectangle(double length, double width) {
         double area = length * width;
 
-        System.out.println("The area of the rectangle is " + area + "\n");
+        System.out.println("The area of the rectangle with length " + length + " and with "+ width + " is equal to " + area + " .\n");
+
     }
 }
