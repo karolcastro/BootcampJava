cbc699382910eadc2b01c72c3aa6b0875107828c
karol castro <karolinycosta@yahoo.com.br>
2022-02-17 18:05:46 -0300

    CarMain

--- src/main/java/com/personal/bootcamp/car/Car.java
+++ src/main/java/com/personal/bootcamp/car/Car.java
@@ -6,6 +6,32 @@ public class Car {
     double price;
     int year;
     String color;
-    
+
+    public Car(String make, double price, int year, String color) {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
 
 }
--- src/main/java/com/personal/bootcamp/car/CarMain.java
+++ src/main/java/com/personal/bootcamp/car/CarMain.java
@@ -2,7 +2,14 @@ package com.personal.bootcamp.car;
 
 public class CarMain {
     public static void main(String[] args) {
+        Car nissan = new Car("Nissan", 50000, 2018, "Black");
 
+        System.out.println("The car make is " + nissan.getMake() + ", is worth $" + nissan.getPrice() +
+                ". It was build in " + nissan.getYear()+ ". It is " + nissan.getColor() + "\n");
 
+        nissan.setColor("blue");
+
+        System.out.println("The car make is " + nissan.getMake() + ", is worth $" + nissan.getPrice() +
+                ". It was build in " + nissan.getYear()+ ". It is " + nissan.getColor() + "\n");
     }
 }
