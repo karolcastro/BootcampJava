22421ad4f1e16dce4a5c38820850a158d68e37ca
karol castro <karolinycosta@yahoo.com.br>
2022-02-17 18:32:10 -0300

    CarMain Getters

--- src/main/java/com/personal/bootcamp/car/Car.java
+++ src/main/java/com/personal/bootcamp/car/Car.java
@@ -1,17 +1,29 @@
 package com.personal.bootcamp.car;
 
+import java.util.Arrays;
+
 public class Car {
 
-    String make;
-    double price;
-    int year;
-    String color;
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+    private String[] parts;
 
-    public Car(String make, double price, int year, String color) {
+    public Car(String make, double price, int year, String color, String[] parts) {
         this.make = make;
         this.price = price;
         this.year = year;
         this.color = color;
+        this.parts = Arrays.copyOf(parts, parts.length);
+    }
+
+    public Car(Car source) {
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+        this.parts = Arrays.copyOf(source.parts, source.parts.length);
     }
 
     public String getMake() {
@@ -28,6 +40,10 @@ public class Car {
 
     public String getColor(){
         return this.color;
+    }
+
+    public String[] getParts(){
+        return this.parts;
     }
 
     public void setColor(String color){
--- src/main/java/com/personal/bootcamp/car/CarMain.java
+++ src/main/java/com/personal/bootcamp/car/CarMain.java
@@ -1,8 +1,13 @@
 package com.personal.bootcamp.car;
 
+import java.util.Arrays;
+
 public class CarMain {
     public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 50000, 2018, "Black");
+
+        String[] parts = {"tires", "Keys"};
+
+        Car nissan = new Car("Nissan", 50000, 2018, "Black", parts );
 
         System.out.println("The car make is " + nissan.getMake() + ", is worth $" + nissan.getPrice() +
                 ". It was build in " + nissan.getYear()+ ". It is " + nissan.getColor() + "\n");
@@ -11,5 +16,7 @@ public class CarMain {
 
         System.out.println("The car make is " + nissan.getMake() + ", is worth $" + nissan.getPrice() +
                 ". It was build in " + nissan.getYear()+ ". It is " + nissan.getColor() + "\n");
+
+        System.out.println(Arrays.toString(nissan.getParts()));
     }
 }
